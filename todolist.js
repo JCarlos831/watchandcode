8550666cb23d854c513f2185cfc844dc753f4c34
Juan Montoya <jcmontoya@gmail.com>
2017-11-26 15:40:46 +0000

    Functions are just like recipes

--- todolist.js
+++ todolist.js
@@ -8,3 +8,16 @@ todos[0] = "item 1 updated";
 
 todos.splice(0,1);
 todos.splice(3, 1);
+
+makeTurkeySandwich
+    Get one slice of bread.
+    Add Turkey.
+    Put a slice of bread on top.
+    
+function makeTurkeySandwich() {
+    Get one slice of bread;
+    Add Turkey;
+    Put a slice of bread on top;
+}
+
+makeTurkeySandwich();
