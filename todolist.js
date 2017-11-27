91aaf097aa23ff72bf932b39d71e21e8cd75eff4
Juan Montoya <jcmontoya@gmail.com>
2017-11-27 18:40:02 +0000

    It should store the todos array on an object

--- todolist.js
+++ todolist.js
@@ -1,33 +1,38 @@
-var todos = ["item 1", "item 2", "item 3"];
-
-function displayTodos(){
-    console.log("My todos: ", todos);
+var todoList = {
+    todos: ["item 1", "item 2", "item 3"],
 }
 
-function addTodo(todo){
-    todos.push(todo);
-    displayTodos();
-}
 
-function changeTodo(position, newValue){
-    todos[position] = newValue;
-    displayTodos();
-}
 
-function deleteTodo(position){
-    todos.splice(position, 1);
-    displayTodos();
-}
 
-var myComputer = {
-    operatingSystem: "mac",
-    screenSize: "15 inches", 
-    purchaseYear: 2011
-};
+// function displayTodos(){
+//     console.log("My todos: ", todos);
+// }
 
-var juan = {
-    name: "Juan",
-    sayName: function(){
-        console.log(this.name)
-    }
-};
+// function addTodo(todo){
+//     todos.push(todo);
+//     displayTodos();
+// }
+
+// function changeTodo(position, newValue){
+//     todos[position] = newValue;
+//     displayTodos();
+// }
+
+// function deleteTodo(position){
+//     todos.splice(position, 1);
+//     displayTodos();
+// }
+
+// var myComputer = {
+//     operatingSystem: "mac",
+//     screenSize: "15 inches", 
+//     purchaseYear: 2011
+// };
+
+// var juan = {
+//     name: "Juan",
+//     sayName: function(){
+//         console.log(this.name)
+//     }
+// };
