61ec4a62d26c766c92ead5c95786bfc103a78f6d
Juan Montoya <jcmontoya@gmail.com>
2017-11-27 22:41:58 +0000

    todoList.toggleCompleted should change the completed property

--- todolist.js
+++ todolist.js
@@ -11,11 +11,16 @@ var todoList = {
         this.displayTodos();
     },
     changeTodo: function(position, todoText){
-        this.todos[position].todoText = todoText,
+        this.todos[position].todoText = todoText;
         this.displayTodos();
     },
     deleteTodo: function(position){
         this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position){
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
         this.displayTodos();
     }
 };
