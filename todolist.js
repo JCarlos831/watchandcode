952c7afad99d71e62f12cb50899a28c3dbc76d9b
Juan Montoya <jcmontoya@gmail.com>
2017-11-27 01:49:00 +0000

    Add V2 Requirements

--- todolist.js
+++ todolist.js
@@ -9,15 +9,34 @@ todos[0] = "item 1 updated";
 todos.splice(0,1);
 todos.splice(3, 1);
 
-makeTurkeySandwich
-    Get one slice of bread.
-    Add Turkey.
-    Put a slice of bread on top.
+// makeTurkeySandwich
+//     Get one slice of bread.
+//     Add Turkey.
+//     Put a slice of bread on top.
     
-function makeTurkeySandwich() {
-    Get one slice of bread;
-    Add Turkey;
-    Put a slice of bread on top;
-}
+// function makeTurkeySandwich() {
+//     Get one slice of bread;
+//     Add Turkey;
+//     Put a slice of bread on top;
+// }
 
-makeTurkeySandwich();
+// makeTurkeySandwich();
+
+// makeSandwichWith_____
+//     Get one slice of bread.
+//     Add _____.
+//     Put a slice of bread on top.
+
+
+// function makeSandwichWith(filling){
+//     Get one slice of bread;
+//     Add filling;
+//     Put a slice of bread on top;
+// }
+
+// makeSandwichWith_ham_
+// makeSandwichWith("ham")
+// console.log("My todos: ", todos)
+function sayHiTo(person){
+    console.log("Hi", person);
+}
