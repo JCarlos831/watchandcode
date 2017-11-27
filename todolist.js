44824fe19dcb3566c7af697d48bea0c5dbd01921
Juan Montoya <jcmontoya@gmail.com>
2017-11-27 18:43:58 +0000

    It should have a displayTodos method

--- todolist.js
+++ todolist.js
@@ -1,13 +1,10 @@
 var todoList = {
     todos: ["item 1", "item 2", "item 3"],
-}
+    displayTodos: function(){
+        console.log("My todos: ", this.todos)
+    }
+};
 
-
-
-
-// function displayTodos(){
-//     console.log("My todos: ", todos);
-// }
 
 // function addTodo(todo){
 //     todos.push(todo);
