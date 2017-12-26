d0b87196a4e90e9afe4e929049ddff46c2dbdf40
Juan Montoya <jcmontoya@gmail.com>
2017-12-26 21:23:23 +0000

    V1 Redo

---
+++ todolist.js
@@ -0,0 +1,6 @@
+var todos = ['item1', 'item2', 'item3']; //it should have a way to store todos
+console.log('My Todos:', todos); //it should have a way to display todos
+todos.push('item4', 'item5'); //it should have a way to add new todos
+todos[0]='item1 updated'; //it should have a way to change a todo
+todos.splice(0,1); //it should have a way to delete a todo (position, how many to delete)
+
