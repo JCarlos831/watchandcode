98756b05ff3e9f88109d99b02ab44dc20314a873
Juan Montoya <jcmontoya@gmail.com>
2017-12-26 23:45:31 +0000

    V2 done

--- todolist.js
+++ todolist.js
@@ -1,47 +1,7 @@
-var todos = ["item 1", "item 2", "item 3"];
+var todos = ['item1', 'item2', 'item3'];
 
-console.log("My todos:", todos);
-
-todos.push("item 4", "item 5");
-
-// todos[0] = "item 1 updated";
-
-// todos.splice(0,1);
-// todos.splice(3, 1);
-
-// makeTurkeySandwich
-//     Get one slice of bread.
-//     Add Turkey.
-//     Put a slice of bread on top.
-    
-// function makeTurkeySandwich() {
-//     Get one slice of bread;
-//     Add Turkey;
-//     Put a slice of bread on top;
-// }
-
-// makeTurkeySandwich();
-
-// makeSandwichWith_____
-//     Get one slice of bread.
-//     Add _____.
-//     Put a slice of bread on top.
-
-
-// function makeSandwichWith(filling){
-//     Get one slice of bread;
-//     Add filling;
-//     Put a slice of bread on top;
-// }
-
-// makeSandwichWith_ham_
-// makeSandwichWith("ham")
-// // console.log("My todos: ", todos)
-// function sayHiTo(person){
-//     console.log("Hi", person);
-// }
 function displayTodos(){
-    console.log("My todos: ", todos);
+    console.log('My todos: ', todos);
 }
 
 function addTodo(todo){
@@ -55,6 +15,6 @@ function changeTodo(position, newValue){
 }
 
 function deleteTodo(position){
-    todos.splice(position, 1);
-    displayTodos();
+	todos.splice(position, 1);
+	displayTodos();
 }
