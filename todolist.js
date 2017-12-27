2d83fe0ccd2fb566eb82ec039049a054307f19a8
Juan Montoya <jcmontoya@gmail.com>
2017-12-27 02:27:16 +0000

    V4 redo completed

--- todolist.js
+++ todolist.js
@@ -1,26 +1,27 @@
 var todoList = {
     todos: [],
     displayTodos: function(){
-        console.log("My todos: ", this.todos);
+        console.log('My todos', this.todos);
     },
     addTodo: function(todoText){
-        this.todos.push({
-            todoText: todoText,
-            completed: false
-        });
-        this.displayTodos();
+      this.todos.push({
+        todoText: todoText,
+        completed: false
+      });
+      this.displayTodos();
     },
     changeTodo: function(position, todoText){
-        this.todos[position].todoText = todoText;
-        this.displayTodos();
+      // this.todos[position] = newValue;
+      this.todos[position].todoText = todoText;
+      this.displayTodos();
     },
     deleteTodo: function(position){
-        this.todos.splice(position, 1);
-        this.displayTodos();
+      this.todos.splice(position, 1);
+      this.displayTodos();
     },
     toggleCompleted: function(position){
-        var todo = this.todos[position];
-        todo.completed = !todo.completed;
-        this.displayTodos();
+      var todo = this.todos[position];
+      todo.completed = !todo.completed;
+      this.displayTodos();
     }
-};
+}
