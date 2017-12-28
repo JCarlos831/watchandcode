c0ae8db558b9c210e099b3ac42e3cdfc2e111cb5
Juan Montoya <jcmontoya@gmail.com>
2017-12-28 02:58:15 +0000

    Version 8 completed

--- todolist.js
+++ todolist.js
@@ -2,7 +2,7 @@ var todoList = {
     todos: [],
     displayTodos: function(){
         if(this.todos.length === 0){
-            console.log('Your todo list is empty');
+            console.log('Your todo list is empty!');
         }else{
             console.log('My todos:');
             for(var i=0; i<this.todos.length;i++){
@@ -55,13 +55,33 @@ var todoList = {
     }
 };
 
-var displayTodosButton = document.getElementById('displayTodosButton');
-var toggleAllButton = document.getElementById('toggleAllButton');
-
-displayTodosButton.addEventListener('click', function(){
-    todoList.displayTodos();
-});
-    
-toggleAllButton.addEventListener('click', function(){
-   todoList.toggleAll(); 
-});
+var handlers = {
+  displayTodos: function(){
+      todoList.displayTodos();
+  },
+  addTodo: function(){
+      var addTodoTextInput = document.getElementById('addTodoTextInput');
+      todoList.addTodo(addTodoTextInput.value);
+      addTodoTextInput.value='';
+  },
+  changeTodo: function(){
+      var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+      var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+      todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+      changeTodoPositionInput.value = '';
+      changeTodoTextInput.value = '';
+  },
+  deleteTodo: function(){
+      var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+      todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+      deleteTodoPositionInput.value = '';
+  },
+  toggleCompleted: function(){
+      var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+      todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+      toggleCompletedPositionInput.value = '';
+  },
+    toggleAll: function(){
+      todoList.toggleAll();
+  }
+};
